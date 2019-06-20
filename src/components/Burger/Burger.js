298aff8933d865f923fa4baac49b3b2b1dd7a7da
Pablo Velez <pvelez@bp-3.com>
2019-06-19 23:45:18 -0500

    Add the burger css, and add the state to the burger builder, so the burger can receive the amount of ingredients from the props

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
+import classes from './Burger.module.css';
 import BurgerIngredients from './BurgerIngredient/BurgerIngredients';
 
 const burger = (props) => {
-    <div>
-        <BurgerIngredients type="meat"></BurgerIngredients>
-    </div>
+    let transformedIngredientsArray = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredients key={igKey + i} type={igKey}/>
+            });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if (transformedIngredientsArray.length === 0) {
+        transformedIngredientsArray = <p>Start Adding Ingredients</p>
+    }
+    return (
+    <div className={classes.Burger}>
+        <BurgerIngredients type="bread-top"></BurgerIngredients>
+        {transformedIngredientsArray}
+        <BurgerIngredients type="bread-bottom"></BurgerIngredients>
+    </div>)
 }
 
 export default burger;

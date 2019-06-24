104c6991d1f02134b32654f9837d0a293d52659c
Pablo Velez <pvelez@bp-3.com>
2019-06-24 14:42:44 -0500

    Add the add ingredients handler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    bacon: 0.7,
+    cheese: 1,
+    meat: 1.3
+}
+
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -10,14 +17,26 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
+        },
+        totalPrice: 4
+    }
+
+    addIngredientsHandler = (type) => {
+        const updatedIngredients = {
+            ...this.state.ingredients
         }
+        updatedIngredients[type] = this.state.ingredients[type] +1;;
+
+        const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+
+        this.setState({ingredients: updatedIngredients, totalPrice: newPrice});
     }
 
     render() {
         return (
             <Aux>
                 <Burger ingredients={ this.state.ingredients }></Burger>
-                <BuildControls></BuildControls>
+                <BuildControls addIngredient={this.addIngredientsHandler}></BuildControls>
             </Aux>
         );
     }

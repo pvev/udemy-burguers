635fca7cd9d6b13524c4cf5fd5e171ab90d2160b
Pablo Velez <pvelez@bp-3.com>
2019-10-09 23:58:36 -0500

    Add the hoc Aux, add the axios instance for http requests, add the spiner component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Modal from '../../components/UI/Modal/Modal';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -22,16 +24,17 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 4,
         purchasable: false,
-        purchasing: false
+        purchasing: false,
+        loading: false
     }
 
     addIngredientsHandler = (type) => {
         const updatedIngredients = {
             ...this.state.ingredients
         }
-        updatedIngredients[type] = this.state.ingredients[type] +1;
+        updatedIngredients[type] = this.state.ingredients[type] + 1;
         const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
-        this.setState({ingredients: updatedIngredients, totalPrice: newPrice});
+        this.setState({ ingredients: updatedIngredients, totalPrice: newPrice });
         this.updatePurchaseState(updatedIngredients);
     }
 
@@ -42,41 +45,71 @@ class BurgerBuilder extends Component {
         const updatedIngredients = {
             ...this.state.ingredients
         }
-        updatedIngredients[type] = this.state.ingredients[type] -1;
+        updatedIngredients[type] = this.state.ingredients[type] - 1;
         const newPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
-        this.setState({ingredients: updatedIngredients, totalPrice: newPrice});
+        this.setState({ ingredients: updatedIngredients, totalPrice: newPrice });
         this.updatePurchaseState(updatedIngredients);
     }
 
     updatePurchaseState = (ingredients) => {
         const sum1 = Object.keys(ingredients)
-                    .map((igKey) => {
-                        return ingredients[igKey]
-                    })
-                    .reduce((sum, el) => {
-                        return sum + el;
-                    }, 0);
-        this.setState({purchasable: sum1 > 0});
+            .map((igKey) => {
+                return ingredients[igKey]
+            })
+            .reduce((sum, el) => {
+                return sum + el;
+            }, 0);
+        this.setState({ purchasable: sum1 > 0 });
     }
 
     purchaseHandler = () => {
-        this.setState({purchasing: true})
+        this.setState({ purchasing: true })
     }
 
     purchaseCancelHandler = () => {
-        this.setState({purchasing: false})
+        this.setState({ purchasing: false })
     }
 
     purchaseContinueHandler = () => {
-        console.log("continue purchase");
+        this.setState({ loading:true });
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Pablo Velez',
+                addres: {
+                    street: 'Test street',
+                    zipCode: '345rwe',
+                    country: 'Spain'
+                },
+                email: 'pablo@genesis.com'
+            },
+            deliveryMethod: 'fastests'
+        }
+        axios.post('/orders.json', order)
+            .then( response => {
+                this.setState({ loading: false, purchasing: false });
+            } )
+            .catch(error => {
+                this.setState({ loading: false, purchasing: false });
+            });
     }
 
     render() {
         const disabledInfo = {
             ...this.state.ingredients
         };
-        for(let key in disabledInfo) {
+        for (let key in disabledInfo) {
             disabledInfo[key] = disabledInfo[key] <= 0;
+        }
+        let orderSummary = <OrderSummary
+            purchaseContinue={this.purchaseContinueHandler}
+            totalPrice={this.state.totalPrice}
+            purchaseCancel={this.purchaseCancelHandler}
+            ingredients={this.state.ingredients}></OrderSummary>
+
+        if (this.state.loading) {
+            orderSummary = <Spinner />
         }
         // disabledInfo = {salad: true, meat: false, ...}
         return (
@@ -84,20 +117,16 @@ class BurgerBuilder extends Component {
                 <Modal
                     modalClosed={this.purchaseCancelHandler}
                     show={this.state.purchasing}>
-                    <OrderSummary
-                        purchaseContinue={this.purchaseContinueHandler}
-                        totalPrice={this.state.totalPrice}
-                        purchaseCancel={this.purchaseCancelHandler}
-                        ingredients={this.state.ingredients}></OrderSummary>
+                    {orderSummary}
                 </Modal>
-                <Burger ingredients={ this.state.ingredients }></Burger>
+                <Burger ingredients={this.state.ingredients}></Burger>
                 <BuildControls
-                totalPrice={this.state.totalPrice}
-                disabled={disabledInfo}
-                removeIngredient={this.removeIngredientsHandler}
-                purchasable={this.state.purchasable}
-                purchasing={this.purchaseHandler}
-                addIngredient={this.addIngredientsHandler}></BuildControls>
+                    totalPrice={this.state.totalPrice}
+                    disabled={disabledInfo}
+                    removeIngredient={this.removeIngredientsHandler}
+                    purchasable={this.state.purchasable}
+                    purchasing={this.purchaseHandler}
+                    addIngredient={this.addIngredientsHandler}></BuildControls>
             </Aux>
         );
     }
